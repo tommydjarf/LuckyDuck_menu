11a637b9dd7669ab83b2cc15093908921acf6a2d
Martin Persson <143416137+Ivan-Georg@users.noreply.github.com>
2023-10-19 08:50:35 +0200

    sortering av allergi, maträtter, ändring av allergier i jason ordnat så add to cart funkar vid filtrering. reset filterknappen är fortfarande kvar om den ska tas bort

--- assets/script/menu.js
+++ assets/script/menu.js
@@ -104,8 +104,10 @@ function setLanguageEventListeners() {
 	const engButton = document.getElementById("eng");
 	const sweButton = document.getElementById("swe");
 
-	engButton.addEventListener("click", () => setLanguage("en"));
-	sweButton.addEventListener("click", () => setLanguage("sv"));
+	engButton.addEventListener("click", () => {setLanguage("en"); clearAllFilters();
+});
+	sweButton.addEventListener("click", () => {setLanguage("sv"); clearAllFilters();
+	});
 }
 
 //Function to set language andu update text
@@ -136,50 +138,223 @@ function start() {
 }
 
 //---------------------------------------------Filter function - test
-var vegetarian = document.getElementById("vegetarian-input").checked;
-var beef = document.getElementById("beef-input").checked;
-var pork = document.getElementById("pork-input").checked;
-var chicken = document.getElementById("chicken-input").checked;
-var fish = document.getElementById("fish-input").checked;
-var gluten = document.getElementById("gluten-input").checked;
-var lactose = document.getElementById("lactose-input").checked;
-var selectedCheckboxes = [];
 
-// async function getMenuItemsBasedOnSelection(language) {
-// 	try {
-// 		const { menu } = await getMenuAndLanguages();
-// 		const items = menu.menu[language];
-// 	} catch (error) {
-// 		console.error("Nu vart de tok med getMenuItems ", error);
-// 	}
-// }
 
-function setupCheckboxEventListener(checkboxId, variable) {
-	var checkbox = document.getElementById(checkboxId);
-	var idWithoutInput = checkboxId.replace("-input", "");
-	checkbox.addEventListener("change", function () {
-		variable = checkbox.checked;
-		if (checkbox.checked) {
-			selectedCheckboxes.push(idWithoutInput);
-		} else {
-			const index = selectedCheckboxes.indexOf(idWithoutInput);
-			if (index > -1) {
-				selectedCheckboxes.splice(index, 1);
-			}
-		}
-		console.log(checkboxId + ": " + variable);
-		console.log(selectedCheckboxes);
-	});
+// Definiera en tom array som heter "data" för att lagra menyobjekten.
+let data = [];
+// En asynkron funktion som hämtar menyobjekten när sidan laddas.
+async function loadData() {
+    try {
+        // Anropa "getMenu()" för att hämta menyn och sätta "data" till menyn för det aktuella språket (currentLanguage).
+        const { menu } = await getMenuAndLanguages();
+        data = menu.menu[language];
+		console.log(data);
+    } catch (error) {
+        // Om det uppstår ett fel, logga det i konsolen.
+        console.error("Något gick fel: ", error);
+    }
 }
 
-setupCheckboxEventListener("vegetarian-input", vegetarian);
-setupCheckboxEventListener("beef-input", beef);
-setupCheckboxEventListener("pork-input", pork);
-setupCheckboxEventListener("chicken-input", chicken);
-setupCheckboxEventListener("fish-input", fish);
-setupCheckboxEventListener("gluten-input", gluten);
-setupCheckboxEventListener("lactose-input", lactose);
+// Anropa "loadData()" när sidan laddas.
+loadData();
+const clearFilterButton = document.getElementById("clearFilter");
+clearFilterButton.addEventListener("click", clearAllFilters);
 
+// Funktion för att återställa alla filter.
+function clearAllFilters() {
+    // tabort checkmarks i alla checkboxar och prisselecten.
+    const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+    checkboxes.forEach((checkbox) => {
+        checkbox.checked = false;
+    });
+    const selectElement = document.getElementById("sortera");
+    selectElement.selectedIndex = 0;
+
+    // Återställ till en tom array.
+    appliedFilters = [];
+
+    // Anropa filterfunktionen för att uppdatera visningen utan filter.
+    const selectedOption = selectElement.value;
+    const filteredData = filterAndSortMenu(selectedOption, appliedFilters);
+    displayMenuItems(filteredData);
+}
+
+// Hitta HTML-elementen för sortering och filtrering.
+const sortera = document.getElementById("sortera");
+const meatFilterContainer = document.getElementById("filterTextBlockMeat");
+const allergiesFilterContainer= document.getElementById("filterTextBlockAllergies");
+// Lägg till händelselyssnare för ändringar i både sortering och filtrering.
+sortera.addEventListener("change", handleSortAndFilter);
+meatFilterContainer.addEventListener("change", handleSortAndFilter);
+allergiesFilterContainer.addEventListener("change", handleSortAndFilter)
+// Funktion för att hantera både filtrering och sortering.
+async function handleSortAndFilter(event) {
+    // Hämta det valda sorteringsalternativet och id på klickad checkbox.
+    const selectedOption = sortera.value;
+    const checkboxId = event.target.value;
+    console.log(checkboxId);
+    if (event.target.type === "checkbox") {
+        const isChecked = event.target.checked;
+        
+        if (isChecked) {
+            // Om en checkbox är markerad, lägg till filtret och logga dess id i konsolen.
+            addFilter(checkboxId);
+			
+        } else {
+            // Om checkboxen inte är markerad, ta bort filtret.
+            removeFilter(checkboxId);
+        }
+    }
+
+    // Använd "appliedFilters" för filtrering och sortering. selectedOption för pris högtlågt
+    const filteredAndSortedData = filterAndSortMenu(selectedOption, appliedFilters);
+
+    // Visa de filtrerade och sorterade rätterna och logga dem i konsolen.
+    displayMenuItems(filteredAndSortedData);
+    console.log(filteredAndSortedData+"här");
+}
+
+// En array som lagrar aktiva filter.
+let appliedFilters = [];
+
+// Funktion för att lägga till ett filter i "appliedFilters".
+function addFilter(filter) {
+    if (!appliedFilters.includes(filter)) {
+        appliedFilters.push(filter);
+        console.log(filter);
+    }
+}
+
+// Funktion för att ta bort ett filter från "appliedFilters".
+function removeFilter(filter) {
+    const index = appliedFilters.indexOf(filter);
+    if (index !== -1) {
+        appliedFilters.splice(index, 1);
+    }
+}
+
+function filterAndSortMenu(selectedOption, filters) {
+	
+	// Dela upp filtren i två kategorier: allergier och mat.
+    const allergyFilters = filters.filter((filter) => filter.startsWith('allergy_'));
+    const foodFilters = filters.filter((filter) => !filter.startsWith('allergy_'));
+
+    // Skapa en filterfunktion som tar hänsyn till både allergier och matfiltret.
+    const combinedFilter = (item) => {
+        // Kontrollera om rätten har alla allergifilter
+        const allergyMatch = allergyFilters.length === 2 || allergyFilters.every((allergyFilter) => item.categories.includes(allergyFilter));
+        
+        // Kontrollera om rätten har något matfilter
+        const foodMatch = foodFilters.length === 0 || foodFilters.some((foodFilter) => item.categories.includes(foodFilter));
+        
+        // Visa rätten om både mat- och allergifilter matchar
+        return allergyMatch && foodMatch;
+    };
+
+    // Använd filterfunktionen för att få de matchande rätterna.
+    const filteredData = data.filter(combinedFilter);
+
+    // Utför sortering baserat på det valda alternativet.
+    if (selectedOption === "priceLowToHigh") {
+        // Sortera "filteredData" i stigande ordning baserat på priset (lägre pris först).
+        filteredData.sort((a, b) => {
+            const aPrice = typeof a.price === "object" ? a.price.full : a.price;
+            const bPrice = typeof b.price === "object" ? b.price.full : b.price;
+            return aPrice - bPrice;
+        });
+    } else if (selectedOption === "priceHighToLow") {
+        // Sortera "filteredData" i fallande ordning baserat på priset (högre pris först).
+        filteredData.sort((a, b) => {
+            const aPrice = typeof a.price === "object" ? a.price.full : a.price;
+            const bPrice = typeof b.price === "object" ? b.price.full : b.price;
+            return bPrice - aPrice;
+        });
+    }
+
+    return filteredData; // Returnera den filtrerade och sorterade datan.
+}
+
+
+
+// Funktion för att filtrera och sortera menyobjekt.
+// function filterAndSortMenu(selectedOption, filters) {
+//     // Logga de aktiva filtren för felsökning.
+//     console.log(filters);
+
+//     // Utför filtrering baserat på aktiva filter.
+//     // Använd Array.filter() för att skapa en ny lista (filteredData) som innehåller endast de objekt som passerar filtreringskriterierna.
+//     const filteredData = data.filter((item) => {
+		
+//         // Om inga filter är aktiva, visa alla rätter.
+//         if (filters.length === 0) {
+// 			return true; // Returnera true för att behålla rätten i filtreringsresultatet.
+        
+// 		}
+//         // Annars, kontrollera om rätten innehåller minst ett av de aktiva filtren.
+//         // Använd Array.some() för att kontrollera om något av de aktiva filtren finns i rättens kategorier.
+//         return filters.some((filter) => item.categories.includes(filter));
+//     });
+
+//     // Utför sortering baserat på det valda alternativet.
+//     if (selectedOption === "priceLowToHigh") {
+//         // Sortera "filteredData" i stigande ordning baserat på priset (lägre pris först).
+//         filteredData.sort((a, b) => {
+//             const aPrice = typeof a.price === "object" ? a.price.full : a.price;
+//             const bPrice = typeof b.price === "object" ? b.price.full : b.price;
+//             return aPrice - bPrice;
+//         });
+//     } else if (selectedOption === "priceHighToLow") {
+//         // Sortera "filteredData" i fallande ordning baserat på priset (högre pris först).
+//         filteredData.sort((a, b) => {
+//             const aPrice = typeof a.price === "object" ? a.price.full : a.price;
+//             const bPrice = typeof b.price === "object" ? b.price.full : b.price;
+//             return bPrice - aPrice;
+//         });
+//     }
+
+//     return filteredData; // Returnera den filtrerade och sorterade datan.
+// }
+
+// Hitta HTML-elementet där menyobjekten ska visas.
+const menuDisplay = document.getElementById("menuDisplay");
+
+// Funktion för att visa menyobjekten.
+function displayMenuItems(items) {
+    // Rensa befintliga menyobjekt.
+    menuDisplay.innerHTML = "";
+
+    // Loopa igenom menyobjekten och skapa div-element för varje objekt.
+    items.forEach(function (item) {
+        const newDiv = document.createElement("div");
+        newDiv.classList.add("menu-item");
+
+        const dishHeader = document.createElement("h3");
+        dishHeader.textContent = item.dish;
+
+        const priceParagraph = document.createElement("p");
+        priceParagraph.classList.add("price");
+
+        // Om priset är ett objekt, visa både halva och hela priserna.
+        if (typeof item.price === "object") {
+            priceParagraph.textContent = `Small: ${item.price.half}:- / Large: ${item.price.full}:-`;
+        } else {
+            priceParagraph.textContent = `${item.price}:-`;
+        }
+
+        const descriptionParagraph = document.createElement("p");
+        descriptionParagraph.classList.add("description");
+        descriptionParagraph.textContent = item.description;
+
+		const addProductButton = createAddToCartButton(language);
+			addProductButton.value = item.id;
+
+        newDiv.appendChild(dishHeader);
+        newDiv.appendChild(priceParagraph);
+        newDiv.appendChild(descriptionParagraph);
+		newDiv.appendChild(addProductButton);
+        menuDisplay.appendChild(newDiv);
+    });
+}
 //-----------------------------------------Cart
 
 let basketItem = [];
